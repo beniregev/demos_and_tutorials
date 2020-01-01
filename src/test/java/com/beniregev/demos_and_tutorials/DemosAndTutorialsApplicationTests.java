411a5802aa0a08e48fbef159a294e75df7be1391
Binyamin Regev <binyamin.regev@nice.com>
2020-01-01 19:19:31 +0200

    Test file

---
+++ src/test/java/com/beniregev/demos_and_tutorials/DemosAndTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beniregev.demos_and_tutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosAndTutorialsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
