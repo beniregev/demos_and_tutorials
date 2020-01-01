d9694b101a172382f382185a87de57b47b5d6fe5
Binyamin Regev <binyamin.regev@nice.com>
2020-01-01 19:18:33 +0200

    root folder files

---
+++ src/main/java/com/beniregev/demos_and_tutorials/DemosAndTutorialsApplication.java
@@ -0,0 +1,13 @@
+package com.beniregev.demos_and_tutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosAndTutorialsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemosAndTutorialsApplication.class, args);
+    }
+
+}
