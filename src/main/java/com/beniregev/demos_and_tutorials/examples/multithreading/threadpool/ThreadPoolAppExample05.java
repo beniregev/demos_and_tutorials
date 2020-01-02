8b7c78ca7cd2c604ec26af7c1c0dbbf630a443b0
Binyamin Regev <binyamin.regev@nice.com>
2020-01-02 12:00:18 +0200

    Multithreading - example 05 CachedThreadPool

---
+++ src/main/java/com/beniregev/demos_and_tutorials/examples/multithreading/threadpool/ThreadPoolAppExample05.java
@@ -0,0 +1,23 @@
+package com.beniregev.demos_and_tutorials.examples.multithreading.threadpool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadPoolAppExample05 {
+    static class Task05 implements Runnable {
+        public void run() {
+            //  Short lived task
+            System.out.println("^^ Thread Name: " + Thread.currentThread().getName());  //  Thread Name: main
+        }
+    }
+
+    public static void main(String[] args) {
+        //  For a lot of short lived tasks
+        ExecutorService service = Executors.newCachedThreadPool();
+        //  Submit the tasks for execution
+        for (int i=0; i < 1000; i++) {
+            service.execute(new Task05());
+        }
+        System.out.println(">> Thread Name: " + Thread.currentThread().getName());  //  Thread Name: Thread-0
+    }
+}
