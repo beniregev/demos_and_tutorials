2d4e9102c0cf7ae277535e1ba83a3a9b61779568
binyamin.regev <beniregev@gmail.com>
2021-01-10 10:45:53 +0200

    Adding class level JavaDoc comment in interface class MailGenerator.

--- src/main/java/com/beniregev/demos_and_tutorials/never_use_switch_example/MailGenerator.java
+++ src/main/java/com/beniregev/demos_and_tutorials/never_use_switch_example/MailGenerator.java
@@ -1,6 +1,14 @@
 package com.beniregev.demos_and_tutorials.never_use_switch_example;
 
 /**
+ * <div>
+ *     <div>This is the interface that all mails will have to implement. </div>
+ *     <ul>Each implementing class will:
+ *     <li>Return a different value in method {@code getMyCode()}. </li>
+ *     <li>Return the message to send in method {@code generateHtml(MailInfo)}. </li>
+ *     </ul>
+ *
+ * </div>
  * @author Binyamin Regev
  * @since 1.8
  */
