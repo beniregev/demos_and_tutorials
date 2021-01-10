2c294d21ec7cdbd2e99c942bf26f612bf0ee8a23
binyamin.regev <beniregev@gmail.com>
2021-01-10 10:52:16 +0200

    Adding class level JavaDoc comment in class MailInfo

--- src/main/java/com/beniregev/demos_and_tutorials/never_use_switch_example/MailInfo.java
+++ src/main/java/com/beniregev/demos_and_tutorials/never_use_switch_example/MailInfo.java
@@ -4,6 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 /**
+ * <div>
+ *     <p>  This class used to pass parameters and values to {@link MailGenerator}
+ *          implementing classes. Currently in this simple example it's only {@code myCode},
+ *          but it can be extended in your implementation.
+ *     </p>
+ * </div>
  * @author Binyamin Regev
  * @since 1.8
  */
