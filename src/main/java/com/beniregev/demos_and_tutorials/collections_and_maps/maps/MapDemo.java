db20fd5aac1bf6623ccc53af0bf9a7591832f4f7
binyamin.regev <beniregev@gmail.com>
2021-01-13 07:18:38 +0200

    Adding class MapDemo in package collections_and_maps

---
+++ src/main/java/com/beniregev/demos_and_tutorials/collections_and_maps/maps/MapDemo.java
@@ -0,0 +1,19 @@
+package com.beniregev.demos_and_tutorials.collections_and_maps.maps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Binyamin Regev
+ * @see java.util.Map
+ * @see java.util.SortedMap
+ * @see java.util.HashMap
+ * @see java.util.LinkedHashMap
+ * @see java.util.Hashtable
+ * @see java.util.TreeMap *
+ */
+public class MapDemo {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+    }
+}
