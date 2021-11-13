af3746d4561741afa9f0a7774e3e043df40166d7
binyamin.regev <beniregev@gmail.com>
2021-11-13 14:46:56 +0200

    Added DFS & TopologicalSort classes, PLUS additional Graph and Vertex class used by DFS & TopologicalSort classes

---
+++ src/main/java/com/beniregev/demos_and_tutorials/examples/algorithms/Graph.java
@@ -0,0 +1,35 @@
+package com.beniregev.demos_and_tutorials.examples.algorithms;
+
+import java.util.Collections;
+
+public class Graph {
+    Vertex[] vertices;
+
+    Graph(boolean[][] matrix){
+        this.vertices = graphFromAdjacencyMatrix(matrix);
+    }
+
+    public Vertex[] graphFromAdjacencyMatrix(boolean[][] matrix){
+        //Add all vertices
+        Vertex[] list = new Vertex[matrix.length];
+        for (int vertex = 0; vertex < matrix.length; vertex++){
+            Vertex newVertex = new Vertex();
+            for (int edge = 0; edge < matrix[vertex].length; edge++){
+                if (matrix[vertex][edge] == true){
+                    newVertex.add(edge);
+                }
+            }
+            list[vertex] = newVertex;
+        }
+
+        // We reverse the order of the edges so that the dfs works as a topological sort.
+        // It makes the stack process vertex A before Vertex B if
+        // encountered at the same time. (Lexicographical Order)
+        for (Vertex v : list){
+            Collections.reverse(v);
+        }
+        return list;
+
+    }
+
+}
---
+++ src/main/java/com/beniregev/demos_and_tutorials/examples/algorithms/Vertex.java
@@ -0,0 +1,6 @@
+package com.beniregev.demos_and_tutorials.examples.algorithms;
+
+import java.util.ArrayList;
+
+public class Vertex extends ArrayList<Integer> {
+}
