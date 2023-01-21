10294e18ca792efd90ca53ae6e58aff30de89fdb
binyamin.regev <beniregev@gmail.com>
2023-01-21 18:12:10 +0200

    Adding a question for Java-Test: Java Regular-Expressions, which of the RegEx Pattern string is the correct one for the requirements?

---
+++ src/main/java/com/beniregev/demos_and_tutorials/demos/java_test/RegularExpressionForPassword.java
@@ -0,0 +1,23 @@
+package com.beniregev.demos_and_tutorials.demos.java_test;
+
+/**
+ * The registration form in the site of a certain organization has certain rules
+ * that must be strictly followed when creating a password to access the site.
+ * Which variable declaration will create a password string that has at least:
+ * <ul>
+ *     <li>One digit.</li>
+ *     <li>One UPPERCASE letter.</li>
+ *     <li>One lowercase letter.</li>
+ *     <li>Password length of 6 to 20 characters.</li>
+ * </ul>
+ */
+public class RegularExpressionForPassword {
+    private static String REGEX_PASSWORD_PATTERN_A = "((?=.[0-9])(?=.[a-z])(?=.[A-Z]).{6,20})";
+    private static String REGEX_PASSWORD_PATTERN_B = "((?=.*d)(?=.*[a-z])(?=.*[A-Z]).(?=.*[6-20]))";
+    private static String REGEX_PASSWORD_PATTERN_C = "((?=.*d)(?=.*[a-z])(?=.*[A-Z]).{6-20})";
+    private static String REGEX_PASSWORD_PATTERN_D = "((?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{6,20})";
+
+    public static void main(String[] args) {
+
+    }
+}
