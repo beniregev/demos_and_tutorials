36238026b771f5217ea827ac4cf6aed8bc0c4cb3
binyamin.regev <beniregev@gmail.com>
2023-01-21 15:42:26 +0200

    Adding question to Java-Test: Is the functional interface correct implementation?

---
+++ src/main/java/com/beniregev/demos_and_tutorials/demos/java_test/ServerStartInterface.java
@@ -0,0 +1,20 @@
+package com.beniregev.demos_and_tutorials.demos.java_test;
+
+/**
+ * Is ServerStartInterface is valid implementation?
+ */
+@FunctionalInterface
+public interface ServerStartInterface {
+    public void startServer();
+    @Override
+    public String toString();
+    @Override
+    public boolean equals(Object o);
+}
+
+class MyClass {
+    public static void main(String[] args) {
+
+    }
+
+}
